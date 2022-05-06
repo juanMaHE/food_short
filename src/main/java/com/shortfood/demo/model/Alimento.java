339f52dc4246c31b81ee7feaa65d585c0a448a07
Juan Manuel <juan.hernandez@enlight.mx>
2022-05-06 17:22:02 -0500

    Se agrega nuevo campo

--- src/main/java/com/shortfood/demo/model/Alimento.java
+++ src/main/java/com/shortfood/demo/model/Alimento.java
@@ -1,9 +1,18 @@
 package com.shortfood.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "alimento", schema = "app_food")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Alimento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,37 +22,7 @@ public class Alimento {
 
     private int id_categoria;
 
+    private String descripcion;
 
-    public Alimento(Long id, String nombre, int id_categoria) {
-        this.id = id;
-        this.nombre = nombre;
-        this.id_categoria = id_categoria;
-    }
 
-    public Alimento() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getId_categoria() {
-        return id_categoria;
-    }
-
-    public void setId_categoria(int id_categoria) {
-        this.id_categoria = id_categoria;
-    }
 }
